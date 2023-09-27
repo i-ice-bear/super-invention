a3d8225ce51568bcf81223ffc28bea293cabfe24
i-ice-bear <andydowsen47us@gmail.com>
2023-09-27 11:11:28 +0530

    Frontend: Added Inter Font in whole App.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import NavbarComponent from "./Components/Navbar";
+import { Inter } from "next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <NavbarComponent />
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <main className={inter.className}>
+      <NextUIProvider>
+        <NavbarComponent />
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </main>
   );
 }
