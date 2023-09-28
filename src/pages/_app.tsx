76244baf8972301912bc14a36ff22eaf621ea884
i-ice-bear <andydowsen47us@gmail.com>
2023-09-28 11:27:30 +0530

    Frontend: Removed Text Animation Banner from project and shifted logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,12 @@ import { NextUIProvider } from "@nextui-org/react";
 import NavbarComponent from "./Components/Navbar";
 import { Inter } from "next/font/google";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
-import {TextAnimate}from "./Components/TextAnimate";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
       <NextUIProvider>
-        <TextAnimate  />
         <NextThemeProvider attribute="class" defaultTheme="light">
           <NavbarComponent />
           <Component {...pageProps} />
