f86db9a618e6ad1e807b81175972027659a5955d
i-ice-bear <andydowsen47us@gmail.com>
2023-09-28 12:42:16 +0530

    Frontend: Added Media and images for the designing and models also

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import HomeComponent from './page/HomePage'
 
 export default function Home() {
   return (
     <>
-    <h1>Billo bagge bediyan raake ki karegi, bagge bagge billo ne ae rakki karegi</h1>
+    <HomeComponent />
     
     </>
   )
---
+++ src/pages/page/HomePage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const HomeComponent = () => {
+  return (
+    <>
+      <div className="wrapper">
+        {/* Hero section container */}
+        <section className="hero-section bg-rose-500  h-auto w-full">
+          <div className="container m-auto w-90 p-24 bg-blue-500">
+            <motion.h1
+              className="text-6xl"
+            >
+              Hi, I'm Obvillion
+            </motion.h1>
+            <h3 className="text-md">
+              A sciene stream student,
+              <br /> who develops web apps and design them
+            </h3>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default HomeComponent;
