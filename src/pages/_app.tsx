a9808328051657c3e6ab981027f7d8867aa9d95b
i-ice-bear <andydowsen47us@gmail.com>
2023-09-29 09:18:34 +0530

    Frontend : Moved Components folder to Home path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
-import NavbarComponent from "./Components/Navbar";
+import NavbarComponent from "../../Components/Navbar";
 import { Inter } from "next/font/google";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 
